33923baf7011be52fbaa5440d0a8ec5ccaed870a
Carlos Montezano <cmontezanol@gmail.com>
2014-11-18 21:30:45 -0200

    adicionando formulario de novo projeto

---
+++ app/control/ProjectForm.class.php
@@ -0,0 +1,18 @@
+<?php
+class ProjectForm extends TPage
+{
+	private $form;
+	private $datagrid;
+	private $pageNavigation;
+	private $loaded;
+
+	public function __construct()
+	{
+		parent::construct();
+	}
+
+	public function onEdit()
+	{
+		//
+	}
+}
